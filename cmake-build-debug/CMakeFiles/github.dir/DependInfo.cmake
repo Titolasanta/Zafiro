bbef1acb97f01addf14bf1f32bb573004761b6dd
OrlandoEspinoza22 <wadawdy.com>
2018-08-31 20:58:56 -0300

    compilacion de cmake

---
+++ cmake-build-debug/CMakeFiles/github.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/orlando/Zafiro/clienteMain.cpp" "/home/orlando/Zafiro/cmake-build-debug/CMakeFiles/github.dir/clienteMain.cpp.o"
+  "/home/orlando/Zafiro/commonButton.cpp" "/home/orlando/Zafiro/cmake-build-debug/CMakeFiles/github.dir/commonButton.cpp.o"
+  "/home/orlando/Zafiro/commonException.cpp" "/home/orlando/Zafiro/cmake-build-debug/CMakeFiles/github.dir/commonException.cpp.o"
+  "/home/orlando/Zafiro/commonImgTexture.cpp" "/home/orlando/Zafiro/cmake-build-debug/CMakeFiles/github.dir/commonImgTexture.cpp.o"
+  "/home/orlando/Zafiro/commonSDLException.cpp" "/home/orlando/Zafiro/cmake-build-debug/CMakeFiles/github.dir/commonSDLException.cpp.o"
+  "/home/orlando/Zafiro/commonSDLIMGException.cpp" "/home/orlando/Zafiro/cmake-build-debug/CMakeFiles/github.dir/commonSDLIMGException.cpp.o"
+  "/home/orlando/Zafiro/commonSDLTTFException.cpp" "/home/orlando/Zafiro/cmake-build-debug/CMakeFiles/github.dir/commonSDLTTFException.cpp.o"
+  "/home/orlando/Zafiro/commonSdlImgInit.cpp" "/home/orlando/Zafiro/cmake-build-debug/CMakeFiles/github.dir/commonSdlImgInit.cpp.o"
+  "/home/orlando/Zafiro/commonSdlInit.cpp" "/home/orlando/Zafiro/cmake-build-debug/CMakeFiles/github.dir/commonSdlInit.cpp.o"
+  "/home/orlando/Zafiro/commonSdlTtfInit.cpp" "/home/orlando/Zafiro/cmake-build-debug/CMakeFiles/github.dir/commonSdlTtfInit.cpp.o"
+  "/home/orlando/Zafiro/commonSocket.cpp" "/home/orlando/Zafiro/cmake-build-debug/CMakeFiles/github.dir/commonSocket.cpp.o"
+  "/home/orlando/Zafiro/commonTextTexture.cpp" "/home/orlando/Zafiro/cmake-build-debug/CMakeFiles/github.dir/commonTextTexture.cpp.o"
+  "/home/orlando/Zafiro/commonTexture.cpp" "/home/orlando/Zafiro/cmake-build-debug/CMakeFiles/github.dir/commonTexture.cpp.o"
+  "/home/orlando/Zafiro/commonWindow.cpp" "/home/orlando/Zafiro/cmake-build-debug/CMakeFiles/github.dir/commonWindow.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/github.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/github.dir/clienteMain.cpp.o"
+  "CMakeFiles/github.dir/commonButton.cpp.o"
+  "CMakeFiles/github.dir/commonException.cpp.o"
+  "CMakeFiles/github.dir/commonImgTexture.cpp.o"
+  "CMakeFiles/github.dir/commonSDLException.cpp.o"
+  "CMakeFiles/github.dir/commonSDLIMGException.cpp.o"
+  "CMakeFiles/github.dir/commonSdlImgInit.cpp.o"
+  "CMakeFiles/github.dir/commonSdlInit.cpp.o"
+  "CMakeFiles/github.dir/commonSDLTTFException.cpp.o"
+  "CMakeFiles/github.dir/commonSdlTtfInit.cpp.o"
+  "CMakeFiles/github.dir/commonSocket.cpp.o"
+  "CMakeFiles/github.dir/commonTextTexture.cpp.o"
+  "CMakeFiles/github.dir/commonTexture.cpp.o"
+  "CMakeFiles/github.dir/commonWindow.cpp.o"
+  "github.pdb"
+  "github"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/github.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
