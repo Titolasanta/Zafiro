bb8773f5f9299658ad0538f9ab176b741fa2c679
BrigliaAn <briglia.antonella@gmail.com>
2018-09-18 17:05:46 -0300

    correcion sprites

--- TP-Fase1-Test/Vista/SpriteRojo.cpp
+++ TP-Fase1-Test/Vista/SpriteRojo.cpp
@@ -17,17 +17,19 @@ SDL_Rect* SpriteRojo::mover(int dirreccion, int frame){
 
     
 SpriteRojo::SpriteRojo()
-: frameMoviemiento { {{1,43,26,35},
-                      {18,43,19,35},
+: frameMoviemiento { {{1,43,16,35}, //Corriendo hacia adelante
+                      {18,43,19,35}, //(segunda linea sprite sheet)
                       {38,47,20,31},
-                      {59,44,19,35},
+                      {59,44,16,34},
                       {76,44,18,34},
                       {95,47,20,31} },
-                     {{1,143,16,35},
-                      {18,149,19,35},
+                     {{1,149,16,35}, //apuntando hacia arriba
+                      {18,149,19,35}, // (quinta linea sprite sheet)
                       {38,151,20,33},
-                      {1,221,20,35},
-                      {22,221,21,35},
-                      {44,223,21,33} } },
-parado{0,5,23,37}
+                      {1,221,20,35}, //apuntando hacia abajo
+                      {22,221,21,35}, // (septima linea sprite sheet)
+                      {44,223,21,33} }
+
+                      },
+parado{0,8,23,34}
                  {}
