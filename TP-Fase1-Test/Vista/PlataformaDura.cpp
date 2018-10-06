f3eab477aedb27ebbc7d36667eacfa246000ce78
FMAA1997 <fede.alvarez1997@gmail.com>
2018-10-05 21:52:51 -0300

    mejoradas las imagenes de hierro y negro

--- TP-Fase1-Test/Vista/PlataformaDura.cpp
+++ TP-Fase1-Test/Vista/PlataformaDura.cpp
@@ -6,7 +6,7 @@
 #include "Logger.h"
 
 extern Logger *gplogger;
-PlataformaDura::PlataformaDura(Window *window) : Viewable(window,"../escenario/hierro.png",0,0,69,36) {
+PlataformaDura::PlataformaDura(Window *window) : Viewable(window,"../escenario/hierro.png",0,0,64,56) {
 
     gplogger->log(3,"Se crea una plataforma dura");
 }
